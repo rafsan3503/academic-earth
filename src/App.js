e520ffec6a29bcf81bf67661105d4b5bec9e1167
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-24 21:16:07 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/router';
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }
