8d86d2c04ed932a1ad80f83d8a72dd954d7d1260
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-25 23:45:18 +0600

    blog changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -189,13 +189,13 @@ const Home = () => {
               </div>
             </div>
             <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-              <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12">
+              <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
                 <p className="relative px-6 py-1 text-lg italic text-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 512 512"
                     fill="currentColor"
-                    className="w-8 h-8"
+                    className="absolute right-0 w-8 h-8 dark:text-violet-400"
                   >
                     <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                     <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
